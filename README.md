dd7aaafe5b132d982a6e63abb0cd4301b837d5f8
Marcus Kida <marcus@kida.io>
2021-05-15 14:35:36 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# DeviceCheck
+
+You may use this tiny module to validate Apple DeviceCheck tokens with the Apple DeviceCheck API.
+
+## Usage
+
+````
+const deviceCheck = require("@bearologics/devicecheck");
+
+const deviceCheckPassed = await deviceCheck(request, {
+    iss: APPLE_JWT_ISS,
+    kid: APPLE_JWT_KID,
+    privateKeyPEM: APPLE_JWT_PRIVATE_KEY,
+});
+
+if (!deviceCheckPassed) {
+    return new Response("Unauthorized", { status: 401 });
+}
+…
+```
+````
+
+## License
+
+[See LICENSE.md](LICENSE.md)
---
+++ index.js
@@ -0,0 +1,41 @@
+const { getToken } = require("@sagi.io/workers-jwt");
+
+module.exports = async function (request, credentials) {
+  const { headers } = request;
+  const { iss, kid, privateKeyPEM } = credentials;
+
+  const jwtPayload = {
+    iss,
+    iat: Math.floor(Date.now() / 1000),
+  };
+
+  const jwt = await getToken({
+    privateKeyPEM,
+    payload: jwtPayload,
+    alg: "ES256",
+    headerAdditions: {
+      kid,
+    },
+  });
+
+  const isSandbox = headers.get("X-Apple-Device-Sandbox") == "true";
+  const deviceToken = headers.get("X-Apple-Device-Token");
+  const environment = isSandbox ? "api.development" : "api";
+
+  const response = await fetch(
+    `https://${environment}.devicecheck.apple.com/v1/validate_device_token`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        device_token: deviceToken,
+        transaction_id: `trns-${Date.now()}`,
+        timestamp: Math.floor(Date.now()),
+      }),
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    }
+  );
+
+  return response.status == 200;
+};
